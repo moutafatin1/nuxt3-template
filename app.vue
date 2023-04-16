cbbdbd798d6a02504ec133e3ba69893755b3e9a4
Oussama Moutafatin <moutafatin3@gmail.com>
2023-04-16 14:21:38 +0000

    custom input component with vee-validate

--- app.vue
+++ app.vue
@@ -1,4 +1,27 @@
+<script lang="ts" setup>
+import { toTypedSchema } from "@vee-validate/zod";
+import { useForm } from "vee-validate";
+import { z } from "zod";
+const { handleSubmit, handleReset } = useForm({
+  validationSchema: toTypedSchema(
+    z.object({
+      test: z.string().min(1, "test is required"),
+    })
+  ),
+});
+
+const testSubmit = handleSubmit((data) => {
+  console.log(data);
+});
+</script>
+
 <template>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <Button>Test</Button>
+  <form
+    @submit="testSubmit"
+    class="mx-6 mt-16 flex max-w-2xl flex-col gap-4 md:mx-auto"
+  >
+    <InputField name="test" placeholder="test value..." />
+    <Button>Submit</Button>
+  </form>
 </template>
